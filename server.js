dc1589d4a6e0f4f39f7cdb242e6b0b40365fb5a8
Philippe Loup <philippe.loup@gmail.com>
2018-04-08 22:00:01 -0400

    Portfolio page and header change

--- server.js
+++ server.js
@@ -39,6 +39,12 @@ app.get('/about',(req,res) => {
   })
 });
 
+app.get('/projects',(req,res) => {
+  res.render('projects.hbs',{
+    pageTitle: 'Projects Portfolio'
+  })
+});
+
 app.get('/',(req,res) => {
   res.render('home.hbs',{
     pageTitle: 'Home Page',
